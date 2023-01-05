f19beefaa227f06c20e2fed11d3367a9596c3b7c
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2023-01-05 12:57:27 +0000

    converts lowercse letters to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/python3
+# function that prints a string in uppercase
+
+
+def uppercase(str):
+    for letter in str:
+        if ord(letter) > 96 and ord(letter) < 123:
+            letter = chr(ord(letter) - 32)
+        print('{}'.format(letter), end='')
+    print('')
