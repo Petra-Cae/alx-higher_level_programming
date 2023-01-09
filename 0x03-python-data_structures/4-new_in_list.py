77d9bac999d80c429b11b805cc8dbbaa995e1f6e
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2023-01-09 15:48:40 +0000

    pycodestyle fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    # Replaces an element in a list at a specific index without modifying the whole list
+    # Replaces an element in a list at a specific index
     if idx < 0 or idx > (len(my_list) - 1):
         return my_list
 
