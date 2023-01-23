16c4737f7c1ee910617e388a45361cc3f229ebaf
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2023-01-23 12:27:33 +0000

    safe print first integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,8 +5,8 @@ def safe_print_list_integers(my_list=[], x=0):
     for el in range(x):
         try:
             print("{:d}".format(my_list[el]), end="")
+            count +=1
         except (TypeError, ValueError):
             continue
-            count += 1
     print()
     return (count)
