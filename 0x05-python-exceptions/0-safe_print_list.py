c521ec32a3a6135c2277d1f5746b5857985f84be
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2023-01-23 12:12:32 +0000

    safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    # prints x elements of a list
+    count = 0
+    for el in range(x):
+        try:
+            print("{}".format(my_list[el]), end="")
+        except IndexError:
+            break
+        else:
+            count += 1
+    print()
+    return (count)
