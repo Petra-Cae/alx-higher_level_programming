c29bc42aa90e55fe72f9c9bdd425ce42deee32d7
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2023-01-23 12:29:41 +0000

    pycodestyle fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for el in range(x):
         try:
             print("{:d}".format(my_list[el]), end="")
-            count +=1
+            count += 1
         except (TypeError, ValueError):
             continue
     print()
