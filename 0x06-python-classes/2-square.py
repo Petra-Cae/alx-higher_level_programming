c4c4ca0679ab40c5ac6727da58695ffe14eed288
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2023-01-26 11:22:16 +0000

    squarely

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-# class Square
+"""defines a class Square"""
 
 
 class Square():
     # represents a square
     def __init__(self, size=0):
-        # init a new square
+        """Initializes a square
+        Args:
+            size (int): size of the square
+        """
         if (type(size) is not int):
             raise TypeError("size must be an integer")
         elif (size < 0):
