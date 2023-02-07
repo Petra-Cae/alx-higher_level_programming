bf5bc765fe18f9714ea1465106e17ef56426b063
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2023-02-07 10:00:15 +0000

    read text and print to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Reads a text file"""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints to stdout
+    Args:
+        filename (str): path to the file to be read
+    """
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+    print(read_data, end="")
