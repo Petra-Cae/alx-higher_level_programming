19bfe7fcea155de01d3f09a84d296b78b0285a0e
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2023-03-16 15:01:03 +0000

    corrected

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates the MySQL server user user_0d_1 with all privileges
 -- The user_0d_1 password will be set
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_id_1_pwd;
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_id_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
