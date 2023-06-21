eeed4811ee2cf132bd17bd573efe0019f89715a3
Petraoje <petraoje4real@gmail.com>
2023-06-21 12:55:18 +0000

    semistandard fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
-const Sq = require('./5-square')
+const Sq = require('./5-square');
 
 class Square extends Sq {
-  charPrint(c) {
+  charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
       for (let a = 0; a < this.height; a++) {
-        console.log(c.repeat(this.width)); }
+        console.log(c.repeat(this.width));
+      }
     }
   }
-};
+}
 
-module.exports = Square
+module.exports = Square;
