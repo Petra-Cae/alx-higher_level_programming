3c7bfe90a0134dd577edb226120138ea6358ac53
Petraoje <petraoje4real@gmail.com>
2023-07-06 08:18:22 +0000

    script length adjustments

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/bash
-# Takes in a URL, sends a GET request to it, and
-# displays the body of the 200 status code response
-
+# Takes in a URL, sends a GET request to it, and displays the body of the 200 status code response
 curl -sL "$1"
