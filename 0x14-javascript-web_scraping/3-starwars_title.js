947806d23628d16d4a3ceb0cf3678cc04dea2bf2
Petraoje <petraoje4real@gmail.com>
2023-07-25 07:28:43 +0000

    match integer to Star Wars movie episode

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/*
+prints the title of a Star Wars movie where the episode number
+matches a given integer
+*/
+const req = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+req.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(JSON.parse(body).title);
+  });
