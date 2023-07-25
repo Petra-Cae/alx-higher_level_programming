175318e8bdc55aa3cc3960fc0c67174293467bf8
Petraoje <petraoje4real@gmail.com>
2023-07-25 06:57:12 +0000

    reads and prints a file's contents

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+//reads and prints the content of a file
+
+const myFile = require('fs');
+
+myFile.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
+});
