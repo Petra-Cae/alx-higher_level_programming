05ce0f429853826fc712161ac086bbc3e670db51
Petraoje <petraoje4real@gmail.com>
2023-07-25 07:20:12 +0000

    displays status code of GET request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// displays the status code of a GET req
+
+const req = require('request');
+
+req.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
